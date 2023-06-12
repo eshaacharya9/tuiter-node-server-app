3f4e689d826fc1871a0e0dade20e002aacfbc908
esha <acharya.e@northeastern.edu>
2023-06-12 11:03:41 -0400

    added update changes

--- users/auth-controller.js
+++ users/auth-controller.js
@@ -1,29 +1,10 @@
 import * as usersDao from "./users-dao.js";
-let users = usersDao;
 
-function AuthenticationController(app){
 
-    const login = (req, res) => { 
-        const username = req.body.username;
-        const password = req.body.password;
-        if(username && password){
-             const user = users.findUserByCredentials(username, password);
-             if (user) {
-                 req.session["currentUser"] = user;
-                 res.json(user);
-             } else {
-                 res.sendStatus(403);
-             }
-        }
-        else {
-         res.sendStatus(403);
-        }
-     
-      };
-
+const AuthController = (app) => {
  const register = (req, res) => {
     const username = req.body.username;
-    const user = users.findUserByUsername(username);
+    const user = usersDao.findUserByUsername(username);
     if (user) {
         res.sendStatus(409);
         return;
@@ -32,7 +13,23 @@ function AuthenticationController(app){
    req.session["currentUser"] = newUser;
    res.json(newUser);
   };
+ const login    = (req, res) => { 
+   const username = req.body.username;
+   const password = req.body.password;
+   if(username && password){
+        const user = usersDao.findUserByCredentials(username, password);
+        if (user) {
+            req.session["currentUser"] = user;
+            res.json(user);
+        } else {
+            res.sendStatus(403);
+        }
+   }
+   else {
+    res.sendStatus(403);
+   }
 
+ };
  const profile  = (req, res) => { 
     const currentUser = req.session["currentUser"];
    if (!currentUser) {
@@ -49,7 +46,21 @@ function AuthenticationController(app){
 
 
  const update   = (req, res) => { 
-    
+    const currentUser = req.session["currentUser"];
+    if (!currentUser) {
+        res.sendStatus(403);
+        return;
+      }
+      const userId = currentUser._id;
+      const updates=req.body;
+      const updatedUser = usersDao.updateUser(userId,updates);
+      if(updatedUser){
+        req.session["currentUser"]=updatedUser;
+        res.json(updatedUser);
+      }
+      else{
+        res.sendStatus(404);
+      }
  };
 
 
